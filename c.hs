0e3a1808cb82fe7e5740dbc26b4bbb61c6860f31
Ankit Kumar Singh <ankitsinghsjs@gmail.com>
2017-02-19 17:35:26 +0530

    Added documentation in c.hs

--- c.hs
+++ c.hs
@@ -8,7 +8,8 @@ map' f (x:xs) = f x : map' f xs
 --
 --foldr' f acc [x] = x f acc
 --foldr' f acc (x:xs) = x f  foldr' f acc xs
-----searching for an item in listi
+----searching for an item in list
+--Quick Sort function
 qsort :: (Ord a) => [a] -> [a]
 qsort [] = []
 qsort (x:xs) =
@@ -16,6 +17,8 @@ qsort (x:xs) =
         equal = [y |y <- (x:xs) , y==x]
 	grtr = [y | y <- (x:xs) , y > x ]
      in qsort small ++ equal  ++  qsort grtr
+--- search' a xs : a function that returns a boolean value if 'a' is found in the list xs then returns True otherwise returns False
 search' a [] = False
 search' a (xs) = if (length ( [ y | y <-xs, y==a] ) > 0) then True else False
-             
+-- getIndex a xs return an integer value : if 'a' is found in list xs then returns its index in xs otherwise returns -1
+getIndex a (xs) = if search' a xs then length ( [ y | y <- xs , y< a]) else -1           
