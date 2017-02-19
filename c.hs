f2370a5a2e20925967db70d255139ad9efce146b
Ankit Kumar Singh <Ankit Kumar Singh>
2017-02-19 17:12:32 +0530

    Added search fn

--- c.hs
+++ c.hs
@@ -8,3 +8,14 @@ map' f (x:xs) = f x : map' f xs
 --
 --foldr' f acc [x] = x f acc
 --foldr' f acc (x:xs) = x f  foldr' f acc xs
+----searching for an item in listi
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) =
+    let small= [y | y <- (x:xs) ,y < x] 
+        equal = [y |y <- (x:xs) , y==x]
+	grtr = [y | y <- (x:xs) , y > x ]
+     in qsort small ++ equal  ++  qsort grtr
+search' a [] = False
+search' a (xs) = if (length ( [ y | y <-xs, y==a] ) > 0) then True else False
+             
