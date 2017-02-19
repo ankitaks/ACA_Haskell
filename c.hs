925e5d9d51332d8a94985bd9cf43b1f15711003e
Ankit Kumar Singh <Ankit Kumar Singh>
2017-02-19 16:17:13 +0530

    modified c.hs

--- c.hs
+++ c.hs
@@ -2,4 +2,9 @@ map' ::  (a -> b)  -> [a] -> [b]
 map' _ [] = [] 
 map' f (x:xs) = f x : map' f xs
 ---folds --- 
-foldl' f acc [] =  acc
+--foldl' f acc [x] =  acc f x
+--foldl' f acc (x:xs) = foldl' f (acc f x ) xs
+---------
+--
+--foldr' f acc [x] = x f acc
+--foldr' f acc (x:xs) = x f  foldr' f acc xs
